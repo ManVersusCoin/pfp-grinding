dc5637151789c43ef134b00a7b60012ebd080f11
ManVersusCoin <98884815+ManVersusCoin@users.noreply.github.com>
2025-04-18 17:18:13 +0000

    v0 commit

--- app/grind/page.tsx
+++ app/grind/page.tsx
@@ -13,7 +13,7 @@ import { SUPPORTED_CHAINS } from '@/lib/constants'
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { Label } from "@/components/ui/label" 
 import {
     Select,
     SelectContent,
