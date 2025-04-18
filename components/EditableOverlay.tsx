b778d2ade30328027ce83ce68db890b1509cd8c0
ManVersusCoin <98884815+ManVersusCoin@users.noreply.github.com>
2025-04-18 10:42:28 +0000

    Back to simple overlay

--- components/EditableOverlay.tsx
+++ components/EditableOverlay.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useState } from 'react'
 import { Rnd } from 'react-rnd'
 
 type Props = {
@@ -8,54 +7,19 @@ type Props = {
 }
 
 export function EditableOverlay({ src }: Props) {
-  const [rotation, setRotation] = useState(0)
-
-  const handleRotate = () => {
-    setRotation((prev) => (prev + 2) % 360)
-  }
-
   return (
-    <div className="relative">
-      <button
-        onClick={handleRotate}
-        className="absolute -top-4 -right-4 bg-blue-600 text-white p-1 rounded-full shadow z-10"
-      >
-        ↻
-      </button>
-
-      <Rnd
-        default={{
-          x: 100,
-          y: 100,
-          width: 150,
-          height: 150,
-        }}
-        bounds="parent"
-        enableResizing
-        lockAspectRatio
-        style={{
-          transform: `rotate(${rotation}deg)`,
-          border: '2px dashed #ccc',
-        }}
-        resizeHandleStyles={{
-          bottomRight: {
-            width: '12px',
-            height: '12px',
-            background: '#333',
-            borderRadius: '2px',
-            position: 'absolute',
-            right: '0',
-            bottom: '0',
-            cursor: 'se-resize',
-          },
-        }}
-      >
-        <img
-          src={src}
-          alt="Overlay"
-          className="w-full h-full object-contain pointer-events-none"
-        />
-      </Rnd>
-    </div>
+    <Rnd
+      default={{
+        x: 100,
+        y: 100,
+        width: 100,
+        height: 100,
+      }}
+      bounds="parent"
+      enableResizing
+      lockAspectRatio
+    >
+      <img src={src} alt="Overlay" className="w-full h-full object-contain pointer-events-none" />
+    </Rnd>
   )
 }
