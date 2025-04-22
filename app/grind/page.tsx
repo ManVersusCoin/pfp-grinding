2393a42b9eaa881feb4ee832190c67d53de280f1
ManVersusCoin <98884815+ManVersusCoin@users.noreply.github.com>
2025-04-22 09:16:00 +0000

    Add OG metadata

--- app/grind/page.tsx
+++ app/grind/page.tsx
@@ -851,3 +851,31 @@ export default function GrindPage() {
         </main>
     );
 }
+
+export const metadata = {
+  title: "Grind Meme Generator / PFP Cutomizer", 
+  description: "Edit your pic or your NFTs by adding $grind overlays!", 
+  icons: {
+    icon: "/overlays/Grind.png", 
+    apple: "/overlays/Grind.png", 
+  },
+  openGraph: {
+    type: "website",
+    title: "Grind Meme Generator / PFP Cutomizer",
+    description: "Edit your pic or your NFTs by adding $grind overlays!",
+    images: [
+      {
+        url: "/overlays/Grind.png", 
+        width: 1200,
+        height: 630,
+        alt: "NFT Editing Tool",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Grind Meme Generator / PFP Cutomizer",
+    description: "Edit your pic or your NFTs by adding $grind overlays!",
+    images: ["/grind-banner.png"], 
+  },
+};
